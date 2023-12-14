5cf05d89decec7942d8dc16ada45086c535456c5
SkylerRo <149194025+SkylerRo@users.noreply.github.com>
2023-12-13 20:13:14 -0500

    sba316_part2

--- index.js
+++ index.js
@@ -1,57 +1,43 @@
 const minNumber = 1;
 const maxNumber = 10;
-const answer = Math.random()
+const answer = Math.floor(Math.random() * (maxNumber - minNumber + 1));
 
 let attempts = 3;
-let guess
-let running = true
+let guess;
+let running = true;
 
 while(running) {
 
     guess = window.prompt (`Pick a Number Between ${minNumber} - ${maxNumber}`);
+    guess = Number(guess)
 
     running = false;
+    
+    if (isNaN(guess)){
+        window.alert("Enter a number funny guy");
+    } 
 
-    if (guess < minNumber){
+    if (guess <= minNumber){
         window.alert("It's definitely more than 1");
     }
 
     if (guess > maxNumber) {
         window.alert("I said less than 10!");
     }
-  
-    if (isNaN(guess)){
-        window.alert("Enter a number funny guy");
-    } 
     else{
         attempts ++;
         if (guess < answer){
-            window.alert("Too low and one guess left to go");
+            window.alert("Too low! Feel the number, be the number.");
         }
     else if (guess > answer){
-        window.alert("Too high, one more try");
+        window.alert("Too high! See the number in your mind.");
     }
    else{
-    window.alert("Wow, you're intuition is strong! Here is a cute baby animal");
+    window.alert("Wow, you're intuition is strong! You should trust your gut always.");
    }
    
    
     }
 
-    const animalList = [
-        {
-            
-            imgSrc: 'https://justsweetdesigns.com/cdn/shop/products/Happyducklingwatermarked.png?v=1683265905&width=1946'
-        },
-        {
-            
-            imgSrc: 'https://cloudfront-us-east-1.images.arcpublishing.com/cmg/2CKDVFHLSZU3GQ53PUJYELEFEE.jpg'
-        },
-        {
-           
-            imgSrc: 'https://static.wixstatic.com/media/93cf71_095dac45aedb46d6aaf1c7f88f8a79d8~mv2.jpg/v1/fill/w_640,h_732,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/93cf71_095dac45aedb46d6aaf1c7f88f8a79d8~mv2.jpg'
-        }
-    
-    ]
 }
 
